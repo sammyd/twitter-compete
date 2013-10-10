c91ac1a4bad8f14be4b6d78b21e68d0a8bc9c96b
Sam Davies <sdavies@shinobicontrols.com>
2013-10-10 14:25:38 +0100

    We now see a stream of competition entry tweets

--- compete-live.rb
+++ compete-live.rb
@@ -5,6 +5,9 @@ require 'json'
 require 'tweetstream'
 
 
+USERNAME = "iwantmyrealname"
+HASHTAG  = "#iOS7DayByDay"
+
 def configure_twitter(config)
     secrets = JSON.parse ( IO.read("secrets.json") )
     secrets = secrets["twitter"]
@@ -20,6 +23,20 @@ TweetStream.configure do |config|
     configure_twitter(config)
 end
 
-TweetStream::Client.new.sample do |status|
-    puts "#{status.text}"
+
+# Get a client
+client = TweetStream::Client.new
+puts client.inspect
+
+client.userstream do |status|
+    
+    if status.retweet?
+        puts "IT'S A RETWEET"
+        if status.retweeted_status.user.screen_name == USERNAME
+            if status.retweeted_status.text.include? HASHTAG
+                if status
+                puts "It's a valid competition entry"
+            end
+        end
+    end
 end
